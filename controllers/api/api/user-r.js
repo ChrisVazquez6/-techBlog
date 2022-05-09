5595a8c1bb7ec54fc61e1c92426d31aede6c0be2
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-09 01:10:36 -0700

    added a login session

--- controllers/api/api/user-r.js
+++ controllers/api/api/user-r.js
@@ -50,3 +50,14 @@ router.post('/login', async (req, res) => {
     res.status(400).json({ message: 'No user account found!' });
   }
 });
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
